72f51db41277b78ef6ef549e5044a15413c53bd2
dihle2020 <38272799+dihle2020@users.noreply.github.com>
2022-07-19 09:05:43 -0700

    Update streamlit_app.py
    pause streamlit to debug

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,6 +33,8 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 
+ # dont run anything past here for troubleshooting
+streamlit.stop()
 
 import snowflake.connector
 
